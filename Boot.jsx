54ae76661fab1926a1a7cdf1da6708813cdaae46
Chamodi Lanka <86542778+Chamodilanka@users.noreply.github.com>
2021-08-10 20:19:40 +0530

    Add files via upload

---
+++ Boot.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1> React Js Bootcamp</h1>
+      <p>
+        This is the last day of the Bootcamp which cover the entire project as
+        it is. And this is the project based on the entire bootcamp program. The
+        instructor of this session was Mr.Shaurya Sinha.
+      </p>
+    </div>
+  );
+}
+
+export default Boot;
